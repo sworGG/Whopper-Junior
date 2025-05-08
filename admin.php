c519fcc0c9a3a9ba6c0f42069a5f876016954471
sworGG <20197ah@mail.ru>
2025-05-08 16:37:31 +0500

    Create admin.php

---
+++ admin.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Админка</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<h1>Управление пользователями</h1>
+<table id="users-table">
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>Имя</th>
+            <th>Роль</th>
+            <th>Действия</th>
+        </tr>
+    </thead>
+    <tbody></tbody>
+</table>
+
+<script src="js/app.js"></script>
+</body>
+</html>
